b2548939d176296ce0e535fb5e609306665e4ab9
Pavarha Maksim <dilexet@gmail.com>
2023-03-11 16:56:47 +0300

    Added figures

---
+++ Geometry.BusinessLogicLayer/Figure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Geometry.BusinessLogicLayer
+{
+    public abstract class Figure
+    {
+        private Lazy<double> _square;
+
+        /// <summary>
+        /// Figure area
+        /// </summary>
+        public double Square
+        {
+            get => _square.Value;
+            protected set => _square = new Lazy<double>(value);
+        }
+
+        protected Figure()
+        {
+            _square = new Lazy<double>(CalculateArea);
+        }
+
+        /// <summary>
+        /// Calculate the area of a figure
+        /// </summary>
+        protected abstract double CalculateArea();
+    }
+}
---
+++ Geometry.BusinessLogicLayer/Implementation/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Geometry.BusinessLogicLayer.CustomException;
+
+namespace Geometry.BusinessLogicLayer.Implementation
+{
+    public class Circle : Figure
+    {
+        private double _radius;
+
+        /// <summary>
+        /// Circle radius
+        /// </summary>
+        /// <exception cref="NegativeNumberException">If the radius of the circle is negative</exception>
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new NegativeNumberException("Radius cannot be negative");
+                }
+
+                _radius = value;
+                Square = CalculateArea();
+            }
+        }
+
+        /// <summary>
+        /// Circle
+        /// </summary>
+        /// <param name="radius">Circle radius</param>
+        /// <exception cref="NegativeNumberException">If the radius of the circle is negative</exception>
+        public Circle(double radius)
+        {
+            if (radius < 0)
+            {
+                throw new NegativeNumberException("Radius cannot be negative");
+            }
+            _radius = radius;
+        }
+
+        /// <summary>
+        /// Calculate the area of the circle
+        /// </summary>
+        protected override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
